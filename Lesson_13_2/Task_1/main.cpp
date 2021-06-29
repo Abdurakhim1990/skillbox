4bc36bb3f6b62f8f63bf8e0c77aeda23fcf7f1dc
Abdurakhim <abdurakhimarada@mail.ru>
2021-06-29 17:05:22 +0300

    Added Lesson_13 Task_1

---
+++ Lesson_13_2/Task_1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> vectorAdd(vector<int> vec, int val)
+{
+    vector<int> newVec(vec.size() + 1);
+
+    for(int i = 0; i < vec.size(); i++){
+        newVec[i] = vec[i];
+    }
+    newVec[vec.size()] = val;
+    return newVec;
+}
+
+int main()
+{
+    int n = 0;
+    vector<int> vec = {5};
+    while(n != -1){
+        cout << "Input ID for robot: ";
+        cin >> n;
+        vec = vectorAdd(vec, n);
+    }
+
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec[i] << endl;
+    }
+
+    return 0;
+}

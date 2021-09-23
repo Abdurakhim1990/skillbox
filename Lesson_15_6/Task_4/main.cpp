f302b476ec7e5f467a5887d8fb4a4bc10fc275a1
Abdurakhim <abdurakhimarada@mail.ru>
2021-09-23 13:32:23 +0300

    Modified Task_4 for Lesson_15_6

--- Lesson_15_6/Task_4/main.cpp
+++ Lesson_15_6/Task_4/main.cpp
@@ -6,22 +6,14 @@ using namespace std;
 int main()
 {
     cout << "Task 4" << endl;
-    //vector<int> vec = {-100,-50, -5, 1, 10, 15};
-    //vector<int> vec = {-100,-50, -40, -25, -15, -5, 0};
-    vector<int> vec = {-1, 2, 10, 35, 51, 110, 150};
+    vector<int> vec = {-91, -56, -25, -9, -1, 2, 10, 35, 51, 110, 150};
     vector<int> mass(vec.size());
-    int start = 0, end = vec.size() - 1;
-    for(int i = vec.size() - 1; i >= 0; i--){
-        if(abs(vec[start]) > abs(vec[end])) {
-            mass[i] = vec[start];
-            start++;
-        } else{
-            mass[i] = vec[end];
-            end--;
-        }
+    int start = 0;
+    for(; start < vec.size() && vec[start] < 0; start++){}
+    int end = start--;
+    while(start >= 0 || end < vec.size()){
+        cout << (start >= 0 && (end == vec.size() || vec[end] > -vec[start]) ? vec[start--] : vec[end++]) << " ";
     }
-    for(int i = 0; i < vec.size(); i++){
-        cout << mass[i] << " ";
-    }
+    cout << endl;
     return 0;
 }

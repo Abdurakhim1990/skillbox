a9906e3e0508acc7f4bc1145839f975e0f20f394
Abdurakhim <abdurakhimarada@mail.ru>
2021-09-23 08:53:02 +0300

    Modified Lesson_15_6 and Task_1 and Task_4

--- Lesson_15_6/Task_1/main.cpp
+++ Lesson_15_6/Task_1/main.cpp
@@ -7,17 +7,22 @@ int main()
 {
     cout << "Task 1" << endl;
     vector<int> mass = {-2, 1, -3, 4, -1, 2, 1, -5, 4, -2, 0, 3, -1, 4, 6};
+    //vector<int> mass = {-2,1,-3,4,-1,2,1,-5,4};
     int max = mass[0] + mass[1];
     int ind_i = 0, ind_j = 1;
-    for(int i = 0; i < mass.size() - 1; i++){
-        int sum = mass[i];
-        for(int j = i + 1; j < mass.size(); j++){
-            sum += mass[j];
-            if(sum > max){
-                max = sum;
-                ind_i = i;
-                ind_j = j;
-            }
+    int sum = 0;
+    for(int i = 0; i < mass.size(); i++){
+        sum += mass[i];
+        if(sum > max){
+            max = sum;
+            ind_j = i;
+        }
+    }
+    for(int i = 0; i < ind_j; i++){
+        sum -= mass[i];
+        if(sum > max){
+            max = sum;
+            ind_i = i + 1;
         }
     }
     cout << "From " << ind_i << " to " << ind_j << endl;

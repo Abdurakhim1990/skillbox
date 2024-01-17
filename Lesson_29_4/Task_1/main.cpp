a5b85a34dfd9e0215eb89162b4b1237f6262a0fe
Abdurakhim <abdurakhimarada@mail.ru>
2024-01-17 10:25:58 +0300

    Modified Lesson 29 second time

--- Lesson_29_4/Task_1/main.cpp
+++ Lesson_29_4/Task_1/main.cpp
@@ -3,23 +3,53 @@
 
 using namespace std;
 
-class Animal
-{
-public:
-    virtual void voice() = 0;
-    string name;
-};
-
 class Talent
 {
 public:
-    virtual void show_talents() = 0;
+    virtual void show_talent() = 0;
 };
 
-class Dog :public Animal, public Talent
+class Dance :public Talent
+{
+public:
+    virtual void show_talent() override
+    {
+        cout << "\tIt can DANCE" << endl;
+    }
+};
+
+class Swim :public Talent
+{
+public:
+    virtual void show_talent() override
+    {
+        cout << "\tIt can SWIM" << endl;
+    }
+};
+
+class Play :public Talent
+{
+public:
+    virtual void show_talent() override
+    {
+        cout << "\tIt can PLAY" << endl;
+    }
+};
+
+class Count :public Talent
+{
+public:
+    virtual void show_talent() override
+    {
+        cout << "\tIt can COUNT" << endl;
+    }
+};
+
+class Dog
 {
 private:
-    vector<string> talents;
+    string name;
+    vector<Talent*> talents;
 public:
     Dog(){};
     Dog(string _name)
@@ -27,62 +57,44 @@ public:
         name = _name;
     };
 
-    virtual void voice() override
-    {
-        cout << "Bark!" << endl;
-    }
-
-    virtual void show_talents() override
+    void show_talents()
     {
         cout << "This is " << name << " and it has some talents:" << endl;
         for(int i = 0; i < talents.size(); ++i){
-            cout << "\tIt can \"" << talents[i] << "\"" << endl;
+            talents[i]->show_talent();
         }
         cout << endl;
     }
 
-    void add_talent(string tal)
+    void add_talent(Talent* tal)
     {
-        for(int i = 0; i < talents.size(); ++i){
-            if(talents[i] == tal)
-                return;
-        }
         talents.push_back(tal);
     }
 };
 
-class Cat : virtual public Animal
-{
-public:
-    virtual void voice()
-    {
-        cout << "Meow!" << endl;
-    }
-};
 
 int main()
 {
-    Talent* dog;// = new Dog("Steve");
+    Play tal_play;
+    Swim tal_swim;
+    Dance tal_dance;
+    Count tal_count;
+
     Dog dog1("Steve");
     Dog dog2("Rex");
     Dog dog3("Sharik");
 
-    dog1.add_talent("dance");
-    dog1.add_talent("swim");
-    dog2.add_talent("dance");
-    dog2.add_talent("play");
-    dog3.add_talent("count");
-    dog3.add_talent("swim");
-    dog3.add_talent("play");
+    dog1.add_talent(&tal_dance);
+    dog1.add_talent(&tal_swim);
+    dog2.add_talent(&tal_dance);
+    dog2.add_talent(&tal_play);
+    dog3.add_talent(&tal_count);
+    dog3.add_talent(&tal_swim);
+    dog3.add_talent(&tal_play);
 
 
-    dog = &dog1;
-    dog->show_talents();
-
-    dog = &dog2;
-    dog->show_talents();
-
-    dog = &dog3;
-    dog->show_talents();
+    dog1.show_talents();
+    dog2.show_talents();
+    dog3.show_talents();
     return 0;
 }

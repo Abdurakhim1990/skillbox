7cbdbb9145c3a1d4b6bb88d859b5e7526c355d00
Abdurakhim <abdurakhimarada@mail.ru>
2024-01-20 14:46:00 +0300

    Modified Lesson_30

--- Lesson_30_4/Task_3/main.cpp
+++ Lesson_30_4/Task_3/main.cpp
@@ -1,9 +1,50 @@
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+//#include <initializer_list>
+#include <cpr/cpr.h>
 
 using namespace std;
 
+
+void PostRequest(map<string, string> &args)
+{
+    vector<cpr::Pair> post_req;
+    for(auto it = args.begin(); it != args.end(); ++it){
+        post_req.push_back(cpr::Pair(it->first, it->second));
+    }
+    auto response = cpr::Post(cpr::Url("https://www.httpbin.org/post"),
+                              cpr::Payload(post_req.begin(), post_req.end())).text;
+    cout << response << endl;
+}
+
+void GetRequest(map<string, string> &args)
+{
+    string get_req = "https://www.httpbin.org/get?";
+    for(auto it = args.begin(); it != args.end(); ++it)
+        get_req += (it->first + "=" + it->second + "&");
+    get_req[get_req.length() - 1] = '\0';
+    auto response = cpr::Get(cpr::Url(get_req)).text;
+    cout << response << endl;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string arg, val;
+    map<string, string> args;
+    cout << "Input ARGUMENT and VALUE" << endl;
+    getline(cin, arg);
+    do {
+        getline(cin, val);
+        args.insert(pair<string, string>(arg, val));
+        cout << "Input ARGUMENT and VALUE" << endl;
+        getline(cin, arg);
+    } while (arg != "post" && arg != "get");
+
+    if(arg == "post"){
+        PostRequest(args);
+    } else {
+        GetRequest(args);
+    }
     return 0;
 }
+

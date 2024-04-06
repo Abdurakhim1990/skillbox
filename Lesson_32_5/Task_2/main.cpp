e82f8f7ba4fe509961edb9abcf696c2decfbab28
Abdurakhim <abdurakhimarada@mail.ru>
2024-04-06 12:06:24 +0300

    Added Lesson_32_5

---
+++ Lesson_32_5/Task_2/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+
+using namespace std;
+
+
+int main()
+{
+    ifstream file_list("../Task_2/movielist.json");
+    if(file_list.is_open()){
+        cout << "Input actor's name: ";
+        string actor_name;
+        getline(cin, actor_name);
+        nlohmann::json json_movie;
+        file_list >> json_movie;
+        for(auto movie = json_movie.begin(); movie != json_movie.end(); ++movie){
+            for(auto cast = json_movie[movie.key()]["cast"].begin(); cast != json_movie[movie.key()]["cast"].end(); ++cast){
+                if(cast.key() == actor_name){
+                    cout << endl << "Movie title: " << movie.key() << endl;
+                    cout << "Actor name: " << cast.key() << endl;
+                    cout << "Actor's role: " << cast.value() << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
--- Lesson_32_5/Test/main.cpp
+++ Lesson_32_5/Test/main.cpp
@@ -1,9 +1,58 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include "nlohmann/json.hpp"
 
 using namespace std;
 
+struct Record
+{
+    string name;
+    string family;
+    int age;
+    bool married;
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    Record record;
+    ifstream file("record.json");
+
+    nlohmann::json dict;
+    file >> dict;
+    record.name = dict["name"];
+    record.family = dict["family"];
+    record.age = dict["age"];
+    record.married = dict["married"];
+    cout << record.name << " " << record.family << " " << record.age << " " << record.married << endl;
+
     return 0;
 }
+
+/*
+int main()
+{
+    Record record;
+//    cin >> record.name >> record.family >> record.age >> record.married;
+//    ofstream file("record.json");
+
+    nlohmann::json dict = "{\"name\":\"Abdurakhim\"}"_json;
+    record.name = dict["name"];
+    cout << record.name;
+
+//    nlohmann::json dict = {
+//        {"name", record.name},
+//        {"family", record.family},
+//        {"age", record.age},
+//        {"married", record.married}
+//    };
+
+//    nlohmann::json dict;
+//    dict["name"] = record.name;
+//    dict["family"] = record.family;
+//    dict["age"] = record.age;
+//    dict["married"] = record.married;
+
+//    file << dict;
+    return 0;
+}*/
